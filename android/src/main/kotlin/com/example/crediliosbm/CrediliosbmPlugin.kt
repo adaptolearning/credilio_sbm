58e7c7baed1ab5eb24027a90ad0dc8f420520165
Vikas Singh <vikas@credilio.in>
2024-06-21 18:33:28 +0530

    lib changes

--- android/src/main/kotlin/com/example/crediliosbm/CrediliosbmPlugin.kt
+++ android/src/main/kotlin/com/example/crediliosbm/CrediliosbmPlugin.kt
@@ -1,35 +1,70 @@
-package com.example.crediliosbm
-
 import androidx.annotation.NonNull
-
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.ActivityResult
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import `in`.co.sbmbank.library.PartnerLibrary
+import `in`.co.sbmbank.library.PartnerLibrarySingleton
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
-/** CrediliosbmPlugin */
-class CrediliosbmPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+// Import your Partner Library classes here
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+class CrediliosbmPlugin : FlutterPlugin, MethodCallHandler {
+  private lateinit var channel: MethodChannel
+  private lateinit var context: Context
+
+  private lateinit var library: PartnerLibrary
+  lateinit var callback: ActivityResultLauncher<Intent>
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "crediliosbm")
     channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
+
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+      "initPartnerLibrary" -> {
+        // Initialize the SDK
+        PartnerLibrarySingleton.init("https://sbmsmartbankinguat.esbeeyem.com:9443")
+        library = PartnerLibrarySingleton.instance
+
+        callback = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+          // Handle the result here
+        }
+
+
+
+      }
+      "openSdkActivity" -> {
+        val token = call.argument<String>("token")
+        val endpoint = call.argument<String>("endpoint")
+
+        if (token != null &&  endpoint!=null) {
+
+          library.open(context, token, endpoint, callback)
+          result.success("Opening SDK activity for endpoint: $endpoint")
+        } else {
+          result.error("PARAMS_ERROR", "Token or endpoint parameter missing", null)
+        }
+      }
+      else -> result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+  private fun handleSdkResult(resultCode: Int) {
+    // Handle SDK result if needed
+    // Example: Implement handling of SDK result callback
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
